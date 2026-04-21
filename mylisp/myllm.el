1a6eaeaf2796a0e4cf0dcf91c96c9a9dfc05780e
Nicolas Vaughan <nivaca@fastmail.com>
2026-04-21 11:13:44 -0500

    llm

--- mylisp/myllm.el
+++ mylisp/myllm.el
@@ -1,29 +1,19 @@
 ;;; mylisp/myllm.el -*- lexical-binding: t; -*-
 
 (add-hook 'find-file-hook #'vc-refresh-state)
+(add-to-list 'vc-handled-backends 'Git)
+(advice-add 'project-root :filter-return #'expand-file-name)
+
+(use-package vterm
+  :straight t)q
 
 (use-package claude-code-ide
   :straight (:type git :host github :repo "manzaltu/claude-code-ide.el")
   :bind ("C-c C-'" . claude-code-ide-menu)
-  :custom
-  (claude-code-ide-terminal-backend 'eat)
-  :config
+  ;; :custom
+  ;; (claude-code-ide-terminal-backend 'eat)
+  ;; :config
   ;; (claude-code-ide-emacs-tools-setup)
-  )
-
-(use-package ellama
-  :disabled
-  :straight t
-  :init
-  (require 'llm-gemini)
-  :config
-  (setq ellama-provider
-        (make-llm-gemini
-         :key "AIzaSyA4GIY0ywdDG5qsdLT0iv_Nx65LZrD91mQ"
-         :chat-model "gemini-3-flash-preview"))
-  (setq ellama-naming-scheme 'ellama-generate-name-by-llm)
-  (setq ellama-keymap-prefix "C-c e")
-  (setq llm-warn-on-nonfree nil)
   )
 
 
